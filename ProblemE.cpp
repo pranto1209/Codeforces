998228734964907a3fe7e3288e05108e0c7069d8
pranto1209 <masumpranto12@gmail.com>
2024-04-17 21:40:09 +0600

    17/4/24

---
+++ ProblemE.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    ll t;
+    cin >> t;
+    while (t--) {
+
+    }
+}
